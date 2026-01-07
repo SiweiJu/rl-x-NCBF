e9b7c96bc86299f8cce6105025d5085960d2409d
Siwei Ju <catherineju@outlook.com>
2026-01-07 13:44:41 +0100

    removed ncbf minibatch_size, always the same as policy minibatch_size

--- rl_x/algorithms/ncbf_ppo/flax_full_jit/default_config.py
+++ rl_x/algorithms/ncbf_ppo/flax_full_jit/default_config.py
@@ -40,7 +40,6 @@ def get_config(algorithm_name):
     config.ncbf.eta_cbf = 1.0 # class Kappa function parameter for CBF constraint
     config.ncbf.use_safety_layer = False
     config.ncbf.nr_minibatches = 50
-    config.ncbf.minibatch_size = 512
     config.ncbf.nr_hidden_units = 512
     config.ncbf.policy_loss_coef = 0.01  # weight for policy loss when training ncbf
     config.ncbf.coef_decay_lambda = 0.95  # decay lambda for ncbf loss coefficients < 1
