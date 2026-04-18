ee6149dea4a3139169fa43be8c7437d56b34513e
Siwei Ju <catherineju@outlook.com>
2026-04-18 17:27:51 +0200

    give velocity ratio to vx, vy and vyaw

--- rl_x/environments/ncbf_mujoco/robot_locomotion/mjx/command_functions/random.py
+++ rl_x/environments/ncbf_mujoco/robot_locomotion/mjx/command_functions/random.py
@@ -12,6 +12,8 @@ class RandomCommands:
         self.all_zero_chance = self.env.env_config["command"]["all_zero_chance"]
         self.single_zero_chance = self.env.env_config["command"]["single_zero_chance"]
 
+        self.velocity_ratio = jnp.array(self.env.env_config["command"]["velocity_ratio"])
+
         self.default_actuator_joint_keep_nominal = np.zeros(env.nr_actuator_joints, dtype=bool)
         self.default_actuator_joint_keep_nominal[env.robot_config["actuator_joints_to_stay_near_nominal"]] = 1.0
         self.default_actuator_joint_keep_nominal = jnp.array(self.default_actuator_joint_keep_nominal)
@@ -24,13 +26,15 @@ class RandomCommands:
     def get_next_command(self, internal_state, should_sample_commands, subkey):
         velocity_sampling_key, all_zeroing_key, single_zeroing_key = jax.random.split(subkey, 3)
 
-        goal_velocities = jax.random.uniform(velocity_sampling_key, (3,), minval=-internal_state["max_command_velocity"], maxval=internal_state["max_command_velocity"])
-        goal_velocities = jnp.where(jnp.abs(goal_velocities) < (self.zero_clip_threshold_percentage * internal_state["max_command_velocity"]), 0.0, goal_velocities)
+        max_command_velocity = self.velocity_ratio * internal_state["max_command_velocity"]
+        goal_velocities = jax.random.uniform(velocity_sampling_key, (3,), minval=-max_command_velocity, maxval=max_command_velocity)
+        jax.debug.print("sampled goal vel: {x}", x=goal_velocities)
+
+        goal_velocities = jnp.where(jnp.abs(goal_velocities) < (self.zero_clip_threshold_percentage * max_command_velocity), 0.0, goal_velocities)
         goal_velocities = jnp.where(jax.random.bernoulli(all_zeroing_key, self.all_zero_chance), jnp.zeros(3), goal_velocities)
         goal_velocities = jnp.where(jax.random.uniform(single_zeroing_key, (3,)) < self.single_zero_chance, 0.0, goal_velocities)
 
         internal_state["goal_velocities"] = jnp.where(should_sample_commands, goal_velocities, internal_state["goal_velocities"])
 
         actuator_joint_keep_nominal = jnp.where(jnp.all(goal_velocities == 0.0), jnp.ones(self.env.nr_actuator_joints, dtype=bool), self.default_actuator_joint_keep_nominal)
-
         internal_state["actuator_joint_keep_nominal"] = jnp.where(should_sample_commands, actuator_joint_keep_nominal, internal_state["actuator_joint_keep_nominal"])
--- rl_x/environments/ncbf_mujoco/robot_locomotion/mjx/default_config.py
+++ rl_x/environments/ncbf_mujoco/robot_locomotion/mjx/default_config.py
@@ -20,6 +20,7 @@ def get_config(environment_name):
             "zero_clip_threshold_percentage": 0.1,
             "all_zero_chance": 0.04,
             "single_zero_chance": 0.005,
+            "velocity_ratio": [1, 0.5, 0.2]
         },
         "env_curriculum_nr_levels": 100,
         "env_curriculum_level_success_episode_return": 8.0,
