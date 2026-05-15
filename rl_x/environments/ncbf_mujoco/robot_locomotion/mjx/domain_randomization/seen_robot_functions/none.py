a816755e5bf36391b1bb0e5e6b02aad97b2bc7c1
Siwei Ju <catherinerju@outlook.com>
2026-05-15 14:22:59 +0200

    Initialize no-DR seen robot state

--- rl_x/environments/ncbf_mujoco/robot_locomotion/mjx/domain_randomization/seen_robot_functions/none.py
+++ rl_x/environments/ncbf_mujoco/robot_locomotion/mjx/domain_randomization/seen_robot_functions/none.py
@@ -4,7 +4,10 @@ class NoneDRSeenRobotFunction:
 
 
     def init(self, internal_state):
-        return
+        internal_state["scaling_factor"] = self.env.scaling_factor
+        internal_state["robot_nominal_qpos_height_over_ground"] = self.env.initial_qpos[2]
+        internal_state["robot_nominal_imu_height_over_ground"] = self.env.initial_imu_height
+        internal_state["nr_collisions_in_nominal"] = 0
 
 
     def sample(self, internal_state, mjx_model, data, should_randomize, key):
--- rl_x/environments/ncbf_mujoco/robot_locomotion/mujoco/domain_randomization/seen_robot_functions/none.py
+++ rl_x/environments/ncbf_mujoco/robot_locomotion/mujoco/domain_randomization/seen_robot_functions/none.py
@@ -4,7 +4,10 @@ class NoneDRSeenRobotFunction:
 
 
     def init(self):
-        return
+        self.env.internal_state["scaling_factor"] = self.env.scaling_factor
+        self.env.internal_state["robot_nominal_qpos_height_over_ground"] = self.env.initial_qpos[2]
+        self.env.internal_state["robot_nominal_imu_height_over_ground"] = self.env.initial_imu_height
+        self.env.internal_state["nr_collisions_in_nominal"] = 0
 
 
     def sample(self):
