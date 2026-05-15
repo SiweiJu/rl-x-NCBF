180708ece9ba0aba572e77bb7d5413a97685e982
Siwei Ju <catherinerju@outlook.com>
2026-05-15 14:28:08 +0200

    Initialize no-DR unseen robot state

--- rl_x/environments/ncbf_mujoco/robot_locomotion/mjx/domain_randomization/unseen_robot_functions/none.py
+++ rl_x/environments/ncbf_mujoco/robot_locomotion/mjx/domain_randomization/unseen_robot_functions/none.py
@@ -1,10 +1,28 @@
+import jax.numpy as jnp
+
+
 class NoneDRUnseenRobotFunction:
     def __init__(self, env):
         self.env = env
 
 
     def init(self, internal_state):
-        return
+        nr_bodies = self.env.initial_mjx_model.body_mass[1:].shape[0]
+        nr_joint_dampings = self.env.initial_mjx_model.dof_damping[6:].shape[0]
+        nr_joint_armatures = self.env.initial_mjx_model.dof_armature[6:].shape[0]
+        nr_joint_stiffnesses = self.env.initial_mjx_model.jnt_stiffness[1:].shape[0]
+        nr_joint_frictionlosses = self.env.initial_mjx_model.dof_frictionloss[6:].shape[0]
+
+        internal_state["mass_inertia_noise_factors"] = jnp.ones(nr_bodies)
+        internal_state["com_noise_factors"] = jnp.ones((nr_bodies, 3))
+        internal_state["body_position_noise_factors"] = jnp.ones((nr_bodies, 3))
+        internal_state["joint_damping_noise_factors"] = jnp.ones(nr_joint_dampings)
+        internal_state["joint_armature_noise_factors"] = jnp.ones(nr_joint_armatures)
+        internal_state["joint_stiffness_noise_factors"] = jnp.ones(nr_joint_stiffnesses)
+        internal_state["joint_friction_loss_noise_factors"] = jnp.ones(nr_joint_frictionlosses)
+        internal_state["p_gain_noise_factors"] = jnp.ones(self.env.nr_actuator_joints)
+        internal_state["d_gain_noise_factors"] = jnp.ones(self.env.nr_actuator_joints)
+        internal_state["position_offsets"] = jnp.zeros(self.env.nr_actuator_joints)
 
 
     def sample(self, internal_state, should_randomize, key):
--- rl_x/environments/ncbf_mujoco/robot_locomotion/mujoco/domain_randomization/unseen_robot_functions/none.py
+++ rl_x/environments/ncbf_mujoco/robot_locomotion/mujoco/domain_randomization/unseen_robot_functions/none.py
@@ -1,10 +1,28 @@
+import numpy as np
+
+
 class NoneDRUnseenRobotFunction:
     def __init__(self, env):
         self.env = env
 
 
     def init(self):
-        return
+        nr_bodies = self.env.initial_mj_model.body_mass[1:].shape[0]
+        nr_joint_dampings = self.env.initial_mj_model.dof_damping[6:].shape[0]
+        nr_joint_armatures = self.env.initial_mj_model.dof_armature[6:].shape[0]
+        nr_joint_stiffnesses = self.env.initial_mj_model.jnt_stiffness[1:].shape[0]
+        nr_joint_frictionlosses = self.env.initial_mj_model.dof_frictionloss[6:].shape[0]
+
+        self.env.internal_state["mass_inertia_noise_factors"] = np.ones(nr_bodies)
+        self.env.internal_state["com_noise_factors"] = np.ones((nr_bodies, 3))
+        self.env.internal_state["body_position_noise_factors"] = np.ones((nr_bodies, 3))
+        self.env.internal_state["joint_damping_noise_factors"] = np.ones(nr_joint_dampings)
+        self.env.internal_state["joint_armature_noise_factors"] = np.ones(nr_joint_armatures)
+        self.env.internal_state["joint_stiffness_noise_factors"] = np.ones(nr_joint_stiffnesses)
+        self.env.internal_state["joint_friction_loss_noise_factors"] = np.ones(nr_joint_frictionlosses)
+        self.env.internal_state["p_gain_noise_factors"] = np.ones(self.env.nr_actuator_joints)
+        self.env.internal_state["d_gain_noise_factors"] = np.ones(self.env.nr_actuator_joints)
+        self.env.internal_state["position_offsets"] = np.zeros(self.env.nr_actuator_joints)
 
 
     def sample(self):
